1589fac6aa279d027de6cbb9b5d0ea7092130576
AlexMikhaylov <alx.mikhaylov@gmail.com>
2023-09-12 20:36:59 +0200

    [MA-25] add user and user repository jpa + local application-local.properties

---
+++ src/main/java/com/example/musicanalyzer/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.musicanalyzer.repositories;
+
+import com.example.musicanalyzer.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // Hier können benutzerdefinierte Abfragemethoden hinzugefügt werden
+}
